ae480997d387c7395eb399ab01ce63c20cdfc742
Christoph Siedentop <christoph.siedentop@st-annes.ox.ac.uk>
2010-09-03 16:50:36 +0200

    added style.css

--- urls.py
+++ urls.py
@@ -1,20 +1,22 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
+import settings
+
 #from django.contrib import admin
 #admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^toolsite/', include('toolsite.foo.urls')),
+	# Example:
+	# (r'^toolsite/', include('toolsite.foo.urls')),
 
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+	# Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+	# to INSTALLED_APPS to enable admin documentation:
+	(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    #(r'^admin/', include(admin.site.urls)),
-    
-    (r'^$', direct_to_template,  { 'template': 'index.html' }, 'index'),
+	# Uncomment the next line to enable the admin:
+	#(r'^admin/', include(admin.site.urls)),
+	(r'^$', direct_to_template,  { 'template': 'index.html' }, 'index'),
 
+	(r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
 )
