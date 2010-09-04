d5c1f2aea42e57cd57e6870b1043c7108117b5f1
Christoph Siedentop <christoph.siedentop@st-annes.ox.ac.uk>
2010-09-04 09:58:54 +0200

    pdfjoin: form can be submitted

--- pdfjoin/utils.py
+++ pdfjoin/utils.py
@@ -4,7 +4,7 @@ import os
 MEDIA_ROOT = "/home/chris/devel/toolsite/media/uploads/"
 UPLOAD_PATH = 'uploads/%Y/%m'
 
-def handle_uploads(request, keys):
+def handle_uploads(FILES):
 	''' Function to handle upload of files '''
 	saved = []
 	
@@ -14,19 +14,20 @@ def handle_uploads(request, keys):
 	if not os.path.exists(upload_full_path):
 		os.makedirs(upload_full_path)
 
-	for key in keys:
-		if key in request.FILES:
-			upload = request.FILES[key]
-			while os.path.exists(os.path.join(upload_full_path, upload.name)):
-				upload.name = '_' + upload.name
-			dest = open(os.path.join(upload_full_path, upload.name), 'wb')
-			for chunk in upload.chunks():
-				dest.write(chunk)
-			dest.close()
-			saved.append((key, os.path.join(upload_dir, upload.name)))
+	for key in FILES:
+		upload = FILES[key]
+		while os.path.exists(os.path.join(upload_full_path, upload.name)):
+			upload.name = '_' + upload.name
+		dest = open(os.path.join(upload_full_path, upload.name), 'wb')
+		for chunk in upload.chunks():
+			dest.write(chunk)
+		dest.close()
+		saved.append((key, os.path.join(upload_dir, upload.name)))
 	# returns [(key1, path1), (key2, path2), ...]
 	return saved
-
+	
+def generate_pdf(files):
+	pass
 
 def is_valid_pdf(filepath):
 	'''Validates that file is proper PDF-File'''
@@ -37,5 +38,3 @@ def is_valid_pdf(filepath):
 	except PdfReadError:
 		return False
 	return True
-	
-	
--- pdfjoin/views.py
+++ pdfjoin/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from django.core.context_processors import csrf
 
 from utils import handle_uploads
+from pdfjoin.models import *
 
+#@csrf_protect
 def show_start(request):
 	'''Shows a start page for PDF-Join. '''
-	return render_to_response('pdfjoin/start.html')
+	if request.method == 'POST':
+		form = FileGroupForm(request.POST, request.FILES)
+		if form.is_valid():
+			files = handle_uploads(request.FILES)
+			filegroup = form.save()
+			output = generate_pdf(files)
+			return HttpResponseRedirect('/')
+	else:
+		form = FileGroupForm()
+	return render_to_response('pdfjoin/start.html', {'form': form }, context_instance=RequestContext(request))
